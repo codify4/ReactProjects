8d0a89c61caf6233205c0a7c7d89f5150b5e1138
IjonK007 <kushta.joni@gmail.com>
2024-02-03 20:04:50 +0100

    Update App.jsx

--- ExpenseTrackerApp/src/App.jsx
+++ ExpenseTrackerApp/src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="h-screen flex flex-col items-center ml-3">
       <h1 className="flex text-6xl font-bold font-sans m-6 p-3">
-        <span className="text-green-700 mr-3">Expense</span> 
+        <span className="bg-gradient-to-r from-green-800 via-green-500 to-green-600 inline-block text-transparent bg-clip-text mr-3">Expense</span> 
         Tracker
       </h1>
       
